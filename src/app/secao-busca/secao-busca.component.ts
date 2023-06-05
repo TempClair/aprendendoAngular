698565b25185c5fecda4c19895b12cab0dd11dcc
StClair_Cl <caehevanras@gmail.com>
2023-06-05 18:00:37 -0300

    Alterações feitas com sucesso. Porém não consegui formatar para cada ItemLinha compor uma <tr> da <table>

--- src/app/secao-busca/secao-busca.component.ts
+++ src/app/secao-busca/secao-busca.component.ts
@@ -1,7 +1,6 @@
 import { Component } from '@angular/core';
 import { InputNumericoComponent } from '../input-numerico/input-numerico.component';
 import { BotaoBuscaComponent } from '../botao-busca/botao-busca.component';
-import { ItemLinha } from '../item-linha/item-linha.component';
 import { ContainerDados } from '../container-dados';
 
 @Component({
@@ -10,5 +9,10 @@ import { ContainerDados } from '../container-dados';
   styleUrls: ['./secao-busca.component.css']
 })
 export class SecaoBuscaComponent {
-  //containerDados : ContainerDados[] = []
+  containerDadosList : ContainerDados[] = [
+    {id:1, nome:"Homer Simpson", cpf:"123.456.789-09"},
+    {id:2, nome:"William Wallace", cpf:"987.654.321-01"},
+    {id:3, nome:"Dora Peterson", cpf:"135.246.357-48"},
+    {id:4, nome:"Vera Walters", cpf:"975.864.753-64"},
+  ]
 }
